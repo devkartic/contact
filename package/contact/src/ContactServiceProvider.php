8ee2561c08f3d5ab2c8b33f39931b3c3681ccee5
dev.kartic <dev.kartic@gmail.com>
2020-03-27 13:30:05 +0600

    create composer.json in package, service provider, route, view

---
+++ package/contact/src/ContactServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace bitfumes\contact;
+use Illuminate\Support\ServiceProvider;
+
+class ContactServiceProvider extends ServiceProvider {
+
+    public function boot(){
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/views', 'contact');
+    }
+
+    public function register()
+    {
+
+    }
+
+}
