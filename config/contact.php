1967db85d02100e211ef8d9b5b2fb6d97c8f95f9
dev.kartic <dev.kartic@gmail.com>
2020-03-29 00:35:00 +0600

    config file add to ContactController and confic, publish add in contactserviceprovider

---
+++ config/contact.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+  'send_email_to' => 'khokon3777@gmail.com'
+];
--- package/contact/src/ContactServiceProvider.php
+++ package/contact/src/ContactServiceProvider.php
@@ -8,7 +8,19 @@ class ContactServiceProvider extends ServiceProvider {
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadViewsFrom(__DIR__.'/views', 'contact');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-        $this->mergeConfigFrom(__DIR__.'/config/mail.php', 'contact');
+        $this->mergeConfigFrom(__DIR__.'/config/contact.php', 'contact');
+        $this->publishes([
+            __DIR__.'/config/contact.php' => config_path('contact.php')
+        ], 'config');
+
+        /*
+         * After add publishes run the following command in terminal
+         *
+         * $ php artisan vendor:publish
+         *
+         * $ index number [Which file should be published]
+         *
+         * */
     }
 
     public function register()
